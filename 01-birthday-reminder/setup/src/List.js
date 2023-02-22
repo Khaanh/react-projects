2ed6ebc83f65cc22635f0eb2ab7badb5d249699e
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-02-22 22:51:23 +0100

    birthday-reminder

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ people }) => {
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <li>
+      {people.map((person) => {
+        const { id, name, image, age } = person;
+
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h3>{name}</h3>
+              <p>years: {age}</p>
+            </div>
+          </article>
+        );
+      })}
+    </li>
   );
 };
 
