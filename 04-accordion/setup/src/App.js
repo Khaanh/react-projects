3c994d4210434faf7f927fff7edf9f1297a5ce9d
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-03-01 08:56:45 +0100

    04-accordion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answer about login</h3>
+
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default App;
